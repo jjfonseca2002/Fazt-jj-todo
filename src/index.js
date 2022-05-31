c731f5ca04013d7537af5d6ae478423844a3d4f4
jjfonseca2002 <jjfonseca2002@yahoo.com>
2022-05-31 11:54:42 -0600

    Corregido el PORT en index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './config'
 import servidor from "./app";
 import './database';
 
-servidor.listen(3000);
-console.log("Server on port", 3000);
+servidor.listen(process.env.PORT || 3000);
+console.log("Server on port", PORT || 3000);
 
 
