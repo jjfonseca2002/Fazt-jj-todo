b232ddbf3689f8ceb7dd432f4ab3712935f72003
jjfonseca2002 <jjfonseca2002@yahoo.com>
2022-05-31 12:00:03 -0600

    corregido el console log(server on port)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import servidor from "./app";
 import './database';
 
 servidor.listen(process.env.PORT || 3000);
-console.log("Server on port", PORT || 3000);
+console.log("Server on port", process.env.PORT || 3000);
 
 
